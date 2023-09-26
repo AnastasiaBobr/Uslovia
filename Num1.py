8f94be2a3b13da6a2a8771f71888557384ac3b00
Anastasia Bobrovskaya <Anastasia Bobrovskaya>
2023-09-26 11:05:35 +0700

    first commit

---
+++ Num1.py
@@ -0,0 +1,11 @@
+a = int(input('vvedite a:'))
+b = int(input('vvedite b:'))
+c = int(input('vvedite c:'))
+
+if a>b and a>c:
+   print(a)
+elif b>a and b>c:
+   print(b)
+elif c>a and c>b:
+   print(c)
+
